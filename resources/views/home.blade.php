082e76020171d6b79d4434ea41489e26c351ef40
ValentinaZussino <vzussino@protonmail.com>
2022-12-15 15:04:07 +0100

    prova stampa comics foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     <h1>hello</h1>
+    {{-- stampa comics --}}
+    <ul>
+        @foreach ($comics as $item)
+        <li>{{$item['title']}}
+            <img src="{{$item['thumb']}}" alt="">
+        </li>
+            
+        @endforeach
+    </ul>
 @endsection
